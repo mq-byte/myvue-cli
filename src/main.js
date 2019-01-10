c3d108137a254a17bf4fdcadae1c76a1ac0a2661
fseqy <1736315428@qq.com>
2019-01-10 22:43:10 +0800

    qy

---
+++ src/main.js
@@ -0,0 +1,3 @@
+const a = 'hello word';
+
+console.log(a);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = {
-    entry: './src/index.js',
+    mode:'production',
+    entry: './src/main.js',
     output: {
-      filename: 'bundle.js',
+      filename: 'app.[hash].js',
       path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins:[
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template:path.resolve(__dirname,'public/index.html')
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000
+      }
 }
